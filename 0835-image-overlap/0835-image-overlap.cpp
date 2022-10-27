daae0ef54cff67a807f1e1c9d008ee4686208a0d
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-10-27 14:02:23 +0530

    Time: 48 ms (89.58%), Space: 10.2 MB (52.08%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-       int n=img1.size();
-        vector<pair<int,int>>vec_a;
-        vector<pair<int,int>>vec_b;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(img1[i][j]==1){
-                    vec_a.push_back({i,j});
+       int M = img1.size(), N = img1[0].size();
+        vector<pair<int,int>> A, B;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (img1[i][j]) {
+                    A.push_back({i, j});
                 }
-                if(img2[i][j]==1){
-                    vec_b.push_back({i,j});
+                if (img2[i][j]) {
+                    B.push_back({i, j});
                 }
             }
         }
-        int ans=0;
-        map<pair<int,int>,int>mp;
-        for(auto [i1,j1]:vec_a){
-            for(auto [i2,j2]:vec_b){
-                mp[{i1-i2,j1-j2}]++;
-                ans=max(ans,mp[{i1-i2,j1-j2}]);
+        
+        vector<vector<int>> match(2*M, vector<int>(2*N, 0));
+        int res = 0;
+        for (auto [i, j] : A) {
+            for (auto [x, y] : B) {
+                res = max(res, ++match[M + i - x][N + j - y]);
             }
         }
-        return ans;
+        return res;
     }
 };
