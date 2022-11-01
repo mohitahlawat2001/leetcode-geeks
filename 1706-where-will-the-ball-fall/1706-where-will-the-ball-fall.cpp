51e2f76243afe10d000a750bf326c77a361685ce
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-11-01 18:56:39 +0530

    Time: 49 ms (63.64%), Space: 13 MB (99.77%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-  int solve(int i, int j, vector<vector<int>>& grid, int m, int n){
-
-        // base condition: if ball passed all rows then, 
-        // ball is out at jth column
-        if(i == m) return j;
+  int solve(int i, int j, vector<vector<int>>& grid){
+        int n = grid[0].size();
+        if(i>=grid.size()) return j;
         
         
-        /* At a time a ball can move only left or right, not both
-		    so even though we have two choices, only one choise can be executed,
-			so the recursive structure is not exponential but rather linear -> O(M)*/
         
-        // ball can move right if current board and next board are towards right
         if(grid[i][j] == 1 && j + 1 < n && grid[i][j + 1] == 1){
-            return solve(i + 1, j + 1, grid, m, n);
+            return solve(i + 1, j + 1, grid);
         }
         
-        // ball can move left if current board and previous board is towards left
         if(grid[i][j] == -1 && j - 1 >= 0 && grid[i][j - 1] == -1){
-            return solve(i + 1, j - 1, grid, m, n);
+            return solve(i + 1, j - 1, grid);
         }
         
-        // if ball couldn't move left or right, then its trapped
         return -1;
     }
     
@@ -34,7 +26,7 @@ public:
         
 		// Find the status of each ball -> O(N)
         for(int i = 0; i < n; i++){
-            fall[i] = solve(0, i, grid, m, n);
+            fall[i] = solve(0, i, grid);
         }
         
         return fall;
