9191b18bb82d6d929547cfcc3357b34a86eebd97
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-11-01 18:57:43 +0530

    Time: 51 ms (59.98%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -3,32 +3,22 @@ public:
   int solve(int i, int j, vector<vector<int>>& grid){
         int n = grid[0].size();
         if(i>=grid.size()) return j;
-        
-        
-        
         if(grid[i][j] == 1 && j + 1 < n && grid[i][j + 1] == 1){
             return solve(i + 1, j + 1, grid);
         }
-        
         if(grid[i][j] == -1 && j - 1 >= 0 && grid[i][j - 1] == -1){
             return solve(i + 1, j - 1, grid);
         }
-        
         return -1;
     }
     
     vector<int> findBall(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        
-        // stores the answer 
-        vector<int> fall(n);
-        
-		// Find the status of each ball -> O(N)
+        vector<int> fall;
         for(int i = 0; i < n; i++){
-            fall[i] = solve(0, i, grid);
+            fall.push_back( solve(0, i, grid));
         }
-        
         return fall;
     }
 };
