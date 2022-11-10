86e7f6c7566133ab041ec1ae800073a25f9e792b
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-11-10 17:40:04 +0530

    Time: 52 ms (99.80%), Space: 25.9 MB (46.90%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+     Solution(){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
     string removeStars(string s) {
         string ans = "";
         
