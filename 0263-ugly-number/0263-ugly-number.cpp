0e8f7c4940ea17b7b87b0dd6e69b83b413b5e0c8
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-11-18 22:51:23 +0530

    Time: 4 ms (41.25%), Space: 5.8 MB (69.56%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isUgly(int num) {
-     for (int i=2; i<6 && num; i++)
-    while (num % i == 0)
-        num /= i;
-return num == 1;
+    if(num==1) return true;
+    if(num==0) return false;
+	while(num%2==0) num=num>>1;
+	while(num%3==0) num=num/3;
+	while(num%5==0) num=num/5;
+    return num==1;
     }
 };
