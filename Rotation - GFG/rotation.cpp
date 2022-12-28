ff07a4fc79b5f752997def59149bb3849cc465ff
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2022-12-28 14:21:12 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -8,28 +8,40 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
+    
+    bool isMin(int nums[],int m,int n){
+        int lval = (m -1)<0 ? INT_MAX:nums[m-1];
+        int rval = (m+1)>=n? INT_MAX:nums[m+1];
+        if(nums[m]<lval && nums[m]<rval) return true;
+        return false;
+    }
+    bool isMax(int nums[],int m,int n){
+        int lval = (m -1)<0 ? INT_MIN:nums[m-1];
+        int rval = (m+1)>=n? INT_MIN:nums[m+1];
+        if(nums[m]>lval && nums[m]>rval) return true;
+        return false;
+    }
+
+   
 	int findKRotation(int nums[], int n) {
 	    // code here
-	    int l = 0, r = n - 1;
-        
-        while (l < r) {
-            int mid = (l + r) / 2;
-
-            if (nums[mid] > nums[r]) {
-                l = mid + 1;
+	     int l =0, r=n-1;
+        if(nums[0]<=nums[r]) return 0;
+        while(r>=l){
+            int m = l + (r-l)/2;
+            if(isMax(nums,m,n)){
+                return m+1;
             }
-            else if (nums[mid] > nums[l]) {
-                r = mid - 1;
-            }
-            else if (nums[mid] < nums[r]) {
-                r = mid;
-            }
-            else if (nums[mid] < nums[l]) {
-                l = mid;
+            else if(isMin(nums,m,n)){
+                return m;
+            } 
+            else if(nums[l]<nums[m]){
+                l = m+1;
+            }else if(nums[r]>nums[m]){
+                r=m-1;
             }
         }
-
-        return l;
+        return 0;
 	}
 
 };
