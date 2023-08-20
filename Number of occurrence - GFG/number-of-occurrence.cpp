2f233729d8cd9877e654db4bbc9a93e52340f8c8
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-08-20 22:14:19 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -1,5 +1,4 @@
 //{ Driver Code Starts
-
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -11,44 +10,16 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-		int first(int nums[],int n, int t){
-        int l=0, r= n-1;
-        int ans =-1;
-        while(r>=l){
-            int m = l + (r-l)/2;
-            if(nums[m]==t){
-                ans = m;
-                r = m-1;
-            } else if(nums[m]>t){
-                r=m-1;
-            } else{
-                l=m+1;
-            }
-        }
-        return ans;
-    }
-     int last(int nums[],int n, int t){
-        int l=0, r= n-1;
-        int ans =-1;
-        while(r>=l){
-            int m = l + (r-l)/2;
-            if(nums[m]==t){
-                ans = m;
-                l = m+1;
-            } else if(nums[m]>t){
-                r=m-1;
-            } else{
-                l=m+1;
-            }
-        }
-        return ans;
-    }
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int firs =first(arr,n,x);
-	    int las = last(arr,n,x);
-	    if(firs == -1) return 0;
-	    return las - firs +1;
+	    int s = lower_bound(arr,arr+n,x)-arr;
+	    if(arr[s]!=x) return 0;
+	    int count =0;
+	    while(arr[s]==x){
+	        count++;
+	        s++;
+	    }
+	    return count;
 	}
 };
 
