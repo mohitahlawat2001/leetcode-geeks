6738342849353bfca6bf3880cf8503abe4ff54ae
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-08-26 16:15:14 +0530

    Added solution - LeetHub

---
+++ LCM And GCD - GFG/lcm-and-gcd.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long gcd(long long a, long long b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        // code here
+        if(A>B){
+            return {(A*B)/gcd(B,A) , gcd(B,A)};
+        }
+        return {(A*B)/gcd(A,B) , gcd(A,B)};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
