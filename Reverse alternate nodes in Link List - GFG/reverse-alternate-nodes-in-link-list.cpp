085c941627bb5e500aec9832e589cd95d63a06cb
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-10-06 22:36:43 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        //add code here
+        Node* temp = odd;
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+        
+        Node* prev = NULL;
+        if(odd->next==NULL) return;
+        Node* curr = odd->next;
+        
+        int flag=0;
+        while(temp){
+            if(flag==0){
+                tail->next = temp;
+                tail = tail->next;
+                temp = temp->next;
+                flag=1;
+            }else{
+                Node* next = temp->next;
+                temp->next = prev; 
+                prev = temp;
+                temp=next;
+            
+                flag=0;
+            }
+        }
+        
+        tail->next = prev;
+        
+        odd = dummy->next;
+        
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
